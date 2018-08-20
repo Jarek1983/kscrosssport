ca9c7b652317ca5fcc3e6e5b916b96febb0b4c08
Jarosław Żmijewski <jzmija@wp.pl>
2018-08-20 16:51:09 +0200

    Stylish - form, added links to navigation

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,5 +1,5 @@
 class Practice < ApplicationRecord
 
-	has_attached_file :picone, styles: {large: "1500x500>", mediun: "500x400>", thumb: "200x150#"}
+	has_attached_file :picone, styles: {large: "1500x500>", medium: "500x400>", thumb: "200x150#"}
 	validates_attachment_content_type :picone, content_type: /\Aimage\/.*\z/
 end
