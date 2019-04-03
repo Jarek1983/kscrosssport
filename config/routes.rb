fdf148115ba3614da0d5e53d57fbedb74e9e81b1
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-04-03 12:46:40 +0200

    Correct seeds

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   		get :practice_list
   	end
   end
+
+resources :carousels
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
