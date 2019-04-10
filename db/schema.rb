ca65a14a311c99809032d214504243e5f3b0ef1e
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-04-10 11:55:16 +0200

    Change sql to psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_30_104818) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carousels", force: :cascade do |t|
     t.string "slidetitle1"
     t.string "slidetitle2"
