bb7e41110eac42047945d5246d2eabc323863652
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-04-11 20:47:39 +0200

    Correct models/carousel

--- app/models/carousel.rb
+++ app/models/carousel.rb
@@ -1,6 +1,7 @@
 class Carousel < ApplicationRecord
 
-	has_attached_file :banner, styles: {large: "1920x800>", medium: "960x400>", small: "480x200#"}
-	validates_attachment_content_type :banner, content_type: /\Aimage\/.*\z/
+	# paperclip
+	# has_attached_file :banner, styles: {large: "1920x800>", medium: "960x400>", small: "480x200#"}
+	# validates_attachment_content_type :banner, content_type: /\Aimage\/.*\z/
 
 end
