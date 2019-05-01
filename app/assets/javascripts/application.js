d1d87994063eb86a264c9b2748cc8d1f52b6b64c
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-05-01 22:02:45 +0200

    Correct footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,10 @@
 //= require activestorage
 //= require turbolinks
 //= require bootstrap-wysihtml5
+//= require bootstrap-wysihtml5/wysihtml5x-toolbar.js
+//= require bootstrap-wysihtml5/handlebars.runtime.min.js
+
+//= require bootstrap-wysihtml5/minimum
 //= require_tree .
 
 
